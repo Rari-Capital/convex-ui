f498c0308c57e02868319764db4934827f2e3e08
nathanhleung <github@nathanhleung.com>
2022-02-24 21:28:36 -0800

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+// Run `ANALYZE=true yarn build` to analyze the bundle
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   webpack: (config, options) => {
     // Transpile rari-components, even though it is in node_modules
     config.module.rules.push({
@@ -9,4 +14,4 @@ module.exports = {
 
     return config;
   },
-};
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,16 @@ import { Provider } from "wagmi";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "rari-components/theme";
 import Layout from "../components/Layout";
-import { connectors } from "utils\/connectors";
+import { connectors } from "utils/connectors";
 import { RariProvider } from "context/RariContext";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools"
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }: any) {
-
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <ChakraProvider theme={theme}>
-      <Provider autoConnect connectors={connectors}>
+      <Provider autoConnect connectors={connectors as any}>
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
           <RariProvider>
