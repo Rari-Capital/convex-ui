10579ef2a27bc8a3e483024c41adc1783ef87c70
Nathan H. Leung <github@nathanhleung.com>
2022-02-25 16:51:20 -0800

    Add assets list template UI to Pool page (#10)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,15 @@
-import { VStack } from '@chakra-ui/react'
-import Header from './Header'
+import { Box } from "@chakra-ui/react";
+import Header from "./Header";
 
 export const Layout = ({ children }: { children: any }) => {
-    return (
-        <VStack w="100%" h="100%">
-            <Header />
-            {children}
-        </VStack>
-    )
-}
+  return (
+    <Box pb={16}>
+      <Header px={16} pb={40} position="relative" zIndex={0} />
+      <Box px={16} mt={-20} position="relative" zIndex={1}>
+        {children}
+      </Box>
+    </Box>
+  );
+};
 
-
-
-export default Layout
+export default Layout;
--- src/components/pages/Pool.tsx
+++ src/components/pages/Pool.tsx
@@ -1,28 +1,46 @@
-import { Heading, Spinner } from '@chakra-ui/react'
-import { usePoolContext } from 'context/PoolContext'
-import { useRari } from 'context/RariContext'
-import usePoolData from 'hooks/pool/usePoolData'
-import { Card } from 'rari-components'
+import { Box, Spinner, VStack } from "@chakra-ui/react";
+import { usePoolContext } from "context/PoolContext";
+import { useRari } from "context/RariContext";
+import usePoolData from "hooks/pool/usePoolData";
+import { Card, Heading } from "rari-components";
 // import { useEffect } from 'react'
 // import { convexAPR } from 'utils/convex/convex2'
 
 const Pool = () => {
-    const { address, provider } = useRari()
-    const { poolInfo, markets } = usePoolContext();
-    // useEffect(() => {
-    //     convexAPR("frax", provider).then((apr) => console.log({ apr }))
-    // }, [])
+  const { address, provider } = useRari();
+  const { poolInfo, markets } = usePoolContext();
+  // useEffect(() => {
+  //     convexAPR("frax", provider).then((apr) => console.log({ apr }))
+  // }, [])
 
-    if (!poolInfo) return <Spinner />
+  if (!poolInfo) return <Spinner />;
 
-    return (
-        <>
-            {
-                !!poolInfo ? <Heading>{poolInfo.name}</Heading> : <Spinner />
-            }
-        </>
-    )
-}
+  return (
+    <Box>
+      <Heading size="md" color="white">
+        Active Positions
+      </Heading>
+      <VStack mt={4} mb={8} align="stretch" spacing={4}>
+        <Card variant="active">
+          <Heading size="lg">UST</Heading>
+        </Card>
+        <Card variant="active">
+          <Heading size="lg">FEI3CRV</Heading>
+        </Card>
+      </VStack>
+      <Heading size="md" color="black">
+        Markets
+      </Heading>
+      <VStack mt={4} align="stretch" spacing={4}>
+        <Card variant="light">
+          <Heading size="lg">FRAX3CRV</Heading>
+        </Card>
+        <Card variant="light">
+          <Heading size="lg">DAI</Heading>
+        </Card>
+      </VStack>
+    </Box>
+  );
+};
 
-export default Pool
-
+export default Pool;
