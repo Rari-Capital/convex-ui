af485bc19b37352fa38a734896d0805250bad851
sharad-s <sharadshekar@gmail.com>
2022-03-02 11:32:34 -0500

    merged koan & main

--- src/components/MarketCard.tsx
+++ src/components/MarketCard.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, VStack } from "@chakra-ui/react";
 import { utils } from "ethers";
-import { StaticData, USDPricedFuseAsset } from "lib/esm/types";
+import { USDPricedFuseAsset } from "lib/esm/types";
 import {
   Badge,
   Button,
@@ -11,19 +11,18 @@ import {
   TokenAmountInput,
   TokenIcon,
 } from "rari-components";
+import { getMillions, convertMantissaToAPY } from "utils/formatters";
 
 type MarketCardProps = Omit<
   React.ComponentProps<typeof ExpandableCard>,
   "expandableChildren"
 > & {
-  marketStaticData: StaticData;
-  marketsDynamicData: USDPricedFuseAsset;
+  marketData: USDPricedFuseAsset;
   type: "supply" | "borrow";
 };
 
 const MarketCard: React.FC<MarketCardProps> = ({
-  marketStaticData,
-  marketsDynamicData,
+  marketData,
   type,
   ...restProps
 }) => {
@@ -35,9 +34,9 @@ const MarketCard: React.FC<MarketCardProps> = ({
         <VStack spacing={4} alignItems="stretch">
           <TokenAmountInput
             variant="light"
-            tokenSymbol={marketStaticData.underlyingSymbol}
-            tokenAddress={marketStaticData.underlyingToken}
-            onClickMax={() => {}}
+            tokenSymbol={marketData.underlyingSymbol}
+            tokenAddress={marketData.underlyingToken}
+            onClickMax={() => { }}
           />
           <StatisticTable
             variant="light"
@@ -52,11 +51,11 @@ const MarketCard: React.FC<MarketCardProps> = ({
       {...restProps}
     >
       <Flex alignItems="center" width="100%">
-        <TokenIcon tokenAddress={marketStaticData.underlyingToken} mr={4} />
+        <TokenIcon tokenAddress={marketData.underlyingToken} mr={4} />
         <Flex direction="column" width="100%">
           <Flex width="auto">
             <Heading size="lg" mr={4}>
-              {marketStaticData.underlyingSymbol}
+              {marketData.underlyingSymbol}
             </Heading>
             <Box alignSelf="center">
               <Badge variant={type === "supply" ? "success" : "warning"}>
@@ -65,19 +64,124 @@ const MarketCard: React.FC<MarketCardProps> = ({
             </Box>
           </Flex>
           <Text variant="secondary" textAlign="left">
-            {parseFloat(utils.formatEther(marketStaticData.collateralFactor)) *
+            {parseFloat(utils.formatEther(marketData.collateralFactor)) *
               100}
             % LTV &middot;
             {utils.formatEther(
-              marketsDynamicData.supplyRatePerBlock.mul(100)
+              marketData.supplyRatePerBlock.mul(100)
             )}{" "}
             Supply APY &middot;
-            {marketsDynamicData.totalSupplyUSD.toString()}M Supplied
+            {marketData.totalSupplyUSD.toString()}M Supplied
           </Text>
         </Flex>
       </Flex>
     </ExpandableCard>
   );
 };
+
+const _MarketCard = ({
+  marketData,
+  type
+}: {
+  marketData: USDPricedFuseAsset
+  type: "supply" | "borrow"
+}) => {
+
+  return (
+    <ExpandableCard
+      width="100%"
+      variant="light"
+      expandableChildren={
+        <VStack spacing={4} alignItems="stretch">
+          <TokenAmountInput
+            variant="light"
+            tokenSymbol={marketData.underlyingSymbol}
+            tokenAddress={marketData.underlyingToken}
+            onClickMax={() => { }}
+          />
+          <StatisticTable
+            variant="light"
+            statistics={[
+              ["Supply Balance", "$24,456"],
+              ["Borrow Limit", "$18,543"],
+            ]}
+          />
+          <Button>Approve</Button>
+        </VStack>
+      }
+    >
+      <Flex alignItems="flex-start" id="hello" width="100%">
+        <TokenIcon tokenAddress={marketData.underlyingToken} mr={4} />
+        <Flex direction="column" width="100%">
+          <Flex direction="column" width="100%">
+            <Flex width="auto">
+              <Heading size="lg" mr={4}>
+                {marketData.underlyingSymbol}
+              </Heading>
+              <Box alignSelf="center">
+                <Badge variant={type === "supply" ? "success" : "warning"}>
+                  {type}
+                </Badge>
+              </Box>
+            </Flex>
+          </Flex>
+          <MarketTLDR
+            marketData={marketData}
+            type={type}
+          />
+        </Flex>
+      </Flex>
+    </ExpandableCard>
+  )
+}
+
+const MarketTLDR = ({
+  marketData,
+  type
+}: {
+  marketData: USDPricedFuseAsset,
+  type: "supply" | "borrow"
+}) => {
+
+  const isSupply = type === "supply"
+  const APY = convertMantissaToAPY(
+    isSupply
+      ? marketData.supplyRatePerBlock
+      : marketData.borrowRatePerBlock, 365
+  )
+
+  const Text1 = isSupply
+    ? `${utils.formatEther(marketData.collateralFactor.mul(100))} LTV`
+    : `${getMillions(marketData.liquidityUSD)} Liquidity`
+
+  // const 
+  return (
+    <Flex justifyContent="flex-start !important">
+      <Text variant="secondary" alignSelf="flex-start" mr="1.5vh">
+        { }% LTV
+      </Text>
+
+      &middot;
+
+      <Text variant="secondary" mr="1.5vh" ml="1.5vh">
+        {Text1}
+      </Text>
+
+      {
+
+        isSupply ? (
+          <>
+            &middot;
+            <Text variant="secondary" ml="1.5vh">
+              {getMillions(marketData.totalSupplyUSD)}M Supplied
+            </Text>
+          </>
+        )
+          : null
+      }
+    </Flex>
+  )
+}
+
 
 export default MarketCard;
---
+++ src/utils/claim.ts
@@ -0,0 +1,60 @@
+export default true
+
+// import { BigNumber, Contract } from "ethers";
+// import LensRouter from "../../contracts/abi/FlywheelLensRouter.json";
+
+// const sri = "0xB290f2F3FAd4E540D0550985951Cdad2711ac34A";
+
+// export type CTokenRewardsByMarket = {
+//   [cTokenAddress: string]: CTokenRewardsForMarket;
+// };
+
+// type CTokenRewardsForMarket = {
+//   [flywheelAddress: string]: BigNumber;
+// };
+
+// export const getUnclaimedRewardsByMarkets = async (
+//   provider: any,
+//   user: string,
+//   markets: string[],
+//   flywheels: string[]
+// ): Promise<CTokenRewardsByMarket> => {
+//   const accrue = new Array(flywheels.length).fill(true);
+//   const claimPlugins = new Array(markets.length).fill(true);
+
+//   const lens = new Contract(
+//     "0x8301bfd36b10e02464ebc64c3362caf18a44203e",
+//     LensRouter,
+//     provider
+//   );
+
+//   let cTokenRewards: CTokenRewardsByMarket = {};
+
+//   markets.forEach(async (market, index) => {
+//     try {
+//       const result: [unclaimed: BigNumber] =
+//         await lens.callStatic.getUnclaimedRewardsForMarket(
+//           user,
+//           market,
+//           flywheels,
+//           accrue,
+//           claimPlugins[index]
+//         );
+//       cTokenRewards[market] = Object.fromEntries(
+//         flywheels.map((k, i) => [k, result[i]])
+//       );
+//     } catch (err) {}
+//   });
+
+//   console.log({
+//     user,
+//     markets,
+//     flywheels,
+//     accrue,
+//     claimPlugins,
+//     lens,
+//     cTokenRewards,
+//   });
+
+//   return cTokenRewards;
+// };
