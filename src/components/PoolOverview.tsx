2cbf5b1f26f655cbfa7b705a01ab5fb7388dea15
Nathan H. Leung <github@nathanhleung.com>
2022-03-07 15:11:47 -0800

    Animate progress bar (#51)

--- src/components/PoolOverview.tsx
+++ src/components/PoolOverview.tsx
@@ -14,8 +14,6 @@ export const PoolOverview: React.FC<BoxProps> = (props) => {
     userHealth: _userHealth,
   } = usePoolContext();
 
-  let userHealth = 80;
-
   // Show total market statistics if user's wallet is not connected.
   const supplyStatisticTitle = !!address ? "You Supplied" : "Total Supplied";
   const borrowStatisticTitle = !!address ? "You Borrowed" : "Total Borrowed";
@@ -29,16 +27,20 @@ export const PoolOverview: React.FC<BoxProps> = (props) => {
       ? marketsDynamicData?.totalBorrowBalanceUSD
       : marketsDynamicData?.totalBorrowedUSD
     )?.toNumber() ?? 0;
+  let borrowPercentageValue = 80;
 
   // Initialize animated values at 0.
   const supplyStatisticInitialValue = useMotionValue(0);
   const borrowStatisticInitialValue = useMotionValue(0);
+  const borrowPercentageInitialValue = useMotionValue(0);
 
   // Store displayed values in state
   const [supplyStatisticDisplayedValue, setSupplyStatisticDisplayedValue] =
     useState(supplyStatisticInitialValue.get());
   const [borrowStatisticDisplayedValue, setBorrowStatisticDisplayedValue] =
     useState(borrowStatisticInitialValue.get());
+  const [borrowPercentageDisplayedValue, setBorrowPercentageDisplayedValue] =
+    useState(borrowPercentageInitialValue.get());
 
   useEffect(() => {
     // Animate displayed values up to actual values. `duration` is in seconds.
@@ -62,19 +64,30 @@ export const PoolOverview: React.FC<BoxProps> = (props) => {
         },
       }
     );
+    const borrowPercentageValueAnimation = animate(
+      borrowPercentageInitialValue,
+      borrowPercentageValue,
+      {
+        duration: 1,
+        onUpdate(value) {
+          setBorrowPercentageDisplayedValue(value);
+        },
+      }
+    );
 
     return () => {
       supplyStatisticValueAnimation.stop();
       borrowStatisticValueAnimation.stop();
+      borrowPercentageValue.stop();
     };
-  }, [supplyStatisticValue, borrowStatisticValue]);
+  }, [supplyStatisticValue, borrowStatisticValue, borrowPercentageValue]);
 
   let color;
-  if (userHealth < 25) {
+  if (borrowPercentageValue < 25) {
     color = "linear-gradient(90.12deg, #4AFA5B 5.91%, #40FFBA 96.27%)";
-  } else if (userHealth < 70) {
+  } else if (borrowPercentageValue < 70) {
     color = "linear-gradient(90.12deg, #40C6FF 5.91%, #4A5BFA 96.27%)";
-  } else { 
+  } else {
     color = "linear-gradient(90.12deg, #F67B36 5.91%, #F64D36 96.27%)";
   }
 
@@ -104,7 +117,7 @@ export const PoolOverview: React.FC<BoxProps> = (props) => {
               <Progress
                 variant="light"
                 barVariant={color}
-                value={userHealth}
+                value={borrowPercentageDisplayedValue}
                 height={4}
               />
             </Card>
